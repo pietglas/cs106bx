4f4467bb0f041051433f7a85d62663971e59f3a1
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-04-03 00:24:26 +0200

    Everything seems to work fine

--- assignment_2/randomwriter/randomwriter.cpp
+++ assignment_2/randomwriter/randomwriter.cpp
@@ -21,11 +21,11 @@ using gram_map = map<queue<string>, vector<string>>;
 gram_map CreateNGrams(ifstream& file, const unsigned int& size) {
 	// position the queue at the beginning of the file
 	gram_map n_grams;
-	queue<string> window;
+	queue<string> key;
 	for (int position = 0; position != size; position++) {
 		string word;
 		file >> word;
-		window.push(word);
+		key.push(word);
 	}
 
 	// loop over the file, repositioning the windows one position at a time
@@ -33,27 +33,17 @@ gram_map CreateNGrams(ifstream& file, const unsigned int& size) {
 	while (file >> new_word) {
 		vector<string> value{new_word};
 		// modify the value, if the key already has one
-		if (!n_grams.try_emplace(window, value).second)
-			n_grams.at(window).push_back(new_word);
-		window.pop();
-		window.push(new_word);
+		if (!n_grams.try_emplace(key, value).second)
+			n_grams.at(key).push_back(new_word);
+		key.pop();
+		key.push(new_word);
 	}
 	return n_grams;
 }
 
-void GenerateRandomText(ifstream& file, ofstream& output,
-						const unsigned int& gramsize, const unsigned int& length) {
-	// find the number of words in the file
-	std::istream_iterator<string> in{file}, end;
-	int wordcount = std::distance(in, end);
-
-	// randomly position the ifstream at some point in the text
-	unsigned int pos = rand() % (wordcount - gramsize);
-	std::istream_iterator<string> position = in;
-	for (int i = 0; i!= pos; i++)
-        ++position;
-
-    // generate the key to start with, write it to output file
+void GenerateRandomText(ifstream& file, ofstream& output, unsigned int wordcount,
+						gram_map grams, int gramsize, const unsigned int& length) {
+	// generate the key to start with, write it to output file
     queue<string> key;
     for (int j = 0; j != gramsize; j++) {
         string word;
@@ -64,8 +54,9 @@ void GenerateRandomText(ifstream& file, ofstream& output,
 
     // randomly select a string from this value and write it to the file
     for (int words_written = 0; words_written != length; words_written++) {
-        vector<string> value = CreateNGrams(file, gramsize).at(key);
-        unsigned int random_pos = rand() & value.size();
+        vector<string> value = grams.at(key);
+        unsigned int random_pos = rand() & (value.size() - 1);
+        cout << "random chosen position: " << random_pos << endl;
         string random_word = value.at(random_pos);
         output << random_word;
 
@@ -78,44 +69,60 @@ void GenerateRandomText(ifstream& file, ofstream& output,
         key.pop();
         key.push(new_word);
     }
+
 }
 
 
 
 int main() {
-	gram_map grams;
-	string file_name = "king_dream.txt";
+	srand(time(nullptr)); // as not to get the same number every time
+
+	// Open file #1: count nr of words 
+	string file_name = "sentence.txt";
 	ifstream file(file_name);
 	while (!file) {
 		cout << "Unable to open file, try again: ";
 		std::getline(std::cin, file_name);
 		file.open(file_name);
 	}
+	std::istream_iterator<string> in{file}, end;
+	unsigned int wordcount = std::distance(in, end);
+	file.close();
+	
+	// Open file #2: get the N-grams
+	ifstream samefile(file_name);
+	while (!samefile) {
+		cout << "Unable to open file, try again: ";
+		std::getline(std::cin, file_name);
+		samefile.open(file_name);
+	}
+    int gramsize = 2;
+    gram_map grams = CreateNGrams(samefile, gramsize);
+    samefile.close();
 
-	ofstream output("randomtext.txt");
+    // Open file to which the output will be written
+    ofstream output("randomtext.txt");
 	if (!output) {
         cout << "could not open a new file, sorry!" << endl;
         exit(1);
 	}
 
-    /*
-	for (auto& key_val:grams) {
-		queue<string> key = key_val.first;
-		cout << "KEY: ";
-		for (int i = 0; i != key.size(); i++) {
-			string front = key.front();
-			cout << front << " ";
-			key.pop();
-			key.push(front);
-		}
-		vector<string> val = key_val.second;
-		cout << "VALUE: ";
-		for (auto it = val.begin(); it != val.end(); ++it) {
-			cout << *it << ", ";
-		}
-		cout << endl;
+    // Open file #3: get random position in the file
+    ifstream file3(file_name);
+	while (!file3) {
+		cout << "Unable to open file, try again: ";
+		std::getline(std::cin, file_name);
+		file3.open(file_name);
 	}
-    */
-    GenerateRandomText(file, output, 2, 10);
+	std::istream_iterator<string> start{file3}, eof;
+	std::istream_iterator<string> iter = in;
+	int pos = std::rand() % (wordcount - gramsize - 1);
+	for (int i = 0; i != pos; i++) {
+        ++start;
+	}
 
-}
+    GenerateRandomText(file3, output, wordcount, grams, gramsize, 5);
+
+    file3.close();
+    output.close();
+}	
