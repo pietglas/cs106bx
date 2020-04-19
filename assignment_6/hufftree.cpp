c887196f31ac1d421e214e812c9e9f279a2f101e
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-04-20 01:57:48 +0200

    added method to decode text, started debugging

--- assignment_6/hufftree.cpp
+++ assignment_6/hufftree.cpp
@@ -1,12 +1,13 @@
-#pragma once
-
 #include <iostream>
 #include <vector>
 #include <map>
 #include <fstream>
 #include <queue>
+#include <cmath>
 #include <string>
 #include "hufftree.h"
+
+using std::cout;    using std::endl;
 
 namespace adt {
 
@@ -20,24 +21,25 @@ PartHuffTree::PartHuffTree(char character, int amount): size_{1} {
 }
 
 PartHuffTree::~PartHuffTree() {
-	PartHuffTree::erase();
+	PartHuffTree::erase(root_);
 }
 
 size_t PartHuffTree::getRootAmount() const {return root_->amount;}
 
-static PartHuffTree*& PartHuffTree::merge(PartHuffTree& first, PartHuffTree& second) {
-	std::unique_ptr<PartHuffTree> new_tree(new PartHuffTree);
-	new_tree->root_->amount = first.amount + second.amount;
-	new_tree->root_->one = first.root_;
-	new_tree->root_->two = second.root_;
+std::unique_ptr<PartHuffTree>& merge(PartHuffTree& first, 
+                                                          PartHuffTree& second) {
+	std::unique_ptr<PartHuffTree> new_tree(new PartHuffTree('\0', 0));
+	new_tree->root_->amount = first.root_->amount + second.root_->amount;
+	new_tree->root_->zero = first.root_;
+	new_tree->root_->one = second.root_;
 
 	return new_tree;
 }
 
-void PartHuffTree::erase(HuffNode*& node=root_) {
+void PartHuffTree::erase(HuffNode*& node) {
 	if (node != nullptr) {
+		PartHuffTree::erase(node->zero);
 		PartHuffTree::erase(node->one);
-		PartHuffTree::erase(node->two);
 		delete node;
 	}
 }
@@ -47,16 +49,16 @@ void PartHuffTree::erase(HuffNode*& node=root_) {
 
 // Count how often each character occurs in a particular text, save
 // as key-value pair in a dictionary. Save the text in a string. 
-void HuffTree::countChars(std::string& file_name) {
+void HuffmanCompress::countChars(std::string& file_name) {
     std::ifstream textfile(file_name);
     if (!textfile) {
-        cout << "An error occurred, maybe the pathfile is wrong?" << endline;
+        cout << "An error occurred, maybe the pathfile is wrong?" << endl;
     }
     else {
-        char character = '';
+        char character = '\0';
         while (textfile.get(character)) {
-            if (!char_occurrences_.try_emplace(std::make_pair(character, 1)).second) {
-                ++char_occurrences_.[character];
+            if (!char_occurrences_.try_emplace(character, 1).second) {
+                ++char_occurrences_[character];
                 text_ += character;     // save text as a string
             }
         }
@@ -65,7 +67,7 @@ void HuffTree::countChars(std::string& file_name) {
 
 // Converts a numerical byte (unsigned char) to a string representing
 // a byte
-std::string HuffTree::convertCharToBitstring(unsigned char nr) {
+std::string HuffmanCompress::convertCharToBitstring(unsigned char nr) const {
     int value = (int)nr;
     std::string byte = "";
     for (int i = 7; i != -1; i--) {
@@ -81,7 +83,7 @@ std::string HuffTree::convertCharToBitstring(unsigned char nr) {
 
 // convert a string representing a byte (for instance, "01001001") 
 // to an actual byte
-unsigned char HuffTree::convertBitstringToChar(std::string byte) {
+unsigned char HuffmanCompress::convertBitstringToChar(std::string byte) const {
     unsigned char c = 0;
     for (int i = 0; i != 8; i++) {
         c <<= 1;
@@ -91,9 +93,11 @@ unsigned char HuffTree::convertBitstringToChar(std::string byte) {
 }
 
 // use the character count to generate a Huffman tree
-void HuffTree::makeEncodeTree() {
-    //PQueueHeap pqueue;  // to do: overload <, ==, >= for PartHuffTree, template PQueueHeap
-    std::priority_queue<PartHuffTree> pqueue;
+void HuffmanCompress::makeEncodeTree() {
+    // PQueueHeap pqueue;  // to do: overload <, ==, >= for PartHuffTree, 
+    // template PQueueHeap
+    
+    std::priority_queue<PartHuffTree> pqueue;   // need to overload < operator
     for (auto& pair : char_occurrences_) {
         PartHuffTree new_tree{pair.first, pair.second};
         pqueue.emplace(new_tree);
@@ -104,37 +108,39 @@ void HuffTree::makeEncodeTree() {
         pqueue.pop();
         PartHuffTree second_elt = pqueue.top();
         pqueue.pop();
-        tree_ptr = PartHuffTree::merge(first_elt, second_elt);
+        tree_ptr = std::move(merge(first_elt, second_elt));    // uses copy constructor
         pqueue.emplace(*tree_ptr);
     }
     tree_ = pqueue.top();
 }
 
 // Generate an encoding dictionary from the Huffman tree
-void HuffTree::makeEncodeMap() {
+void HuffmanCompress::makeEncodeDecodeMaps() {
     HuffNode* ctr = tree_.root_;
     std::string bits = "";
-    if (ctr->zero == nullptr && ctr->one == nullptr) 
+    if (ctr->zero == nullptr && ctr->one == nullptr) {
         encoding_map_.emplace(std::make_pair(ctr->character, bits));
+        decoding_map_.emplace(std::make_pair(bits, ctr->character));
+    }
     else {
         if (ctr->zero != nullptr) {
             ctr = ctr->zero;
             bits += '0';
-            HuffTree::makeEncodeMap();
-            bits -= '0';
+            HuffmanCompress::makeEncodeDecodeMaps();
+            bits.pop_back();
         }
         if (ctr->one != nullptr) {
             ctr = ctr->one;
             bits += '1';
-            HuffTree::makeEncodeMap();
-            bits -= '1';
+            HuffmanCompress::makeEncodeDecodeMaps();
+            bits.pop_back();
         }
     }
 }
 
-void HuffTree::encodeText() {
+void HuffmanCompress::encodeText() {
     // convert text to a string of 0's and 1's, using Huffman encoding
-    for (character : text_) {
+    for (auto& character : text_) {
         // for each character, determine its encoding
         std::string value = encoding_map_[character];
         encoded_text_ += value;
@@ -145,14 +151,14 @@ void HuffTree::encodeText() {
 }
 
 // Encode the text using our dictionary and save it to a binary file
-void HuffTree::safeEncodedText(std::string compressed_file_name) const {
-    std::queue characters;
+void HuffmanCompress::safeEncodedText(std::string compressed_file_name) const {
+    std::queue<char> characters;
     // reserve memory for the encoded text
     size_t size_file = encoded_text_.size() / 8;
     unsigned char * encoded_text_as_bytes = new unsigned char [size_file];
     // keep track of the index
-    ctr = 0;    
-    for (bit : encoded_text_) {
+    size_t ctr = 0;    
+    for (auto& bit : encoded_text_) {
         characters.push(bit);
         // if we added 8 'bits' to the queue, make a new byte
         if (characters.size() == 8) {
@@ -163,24 +169,56 @@ void HuffTree::safeEncodedText(std::string compressed_file_name) const {
                 characters.pop();
             }
             // convert the string representing the byte to an actual byte
-            unsigned char byte = HuffTree::convertBitstringToChar(str_byte);
-            encoded_text_[ctr] = byte;
+            unsigned char byte = HuffmanCompress::convertBitstringToChar(str_byte);
+            encoded_text_as_bytes[ctr] = byte;
             ++ctr;
         }
     }
     // write the bytes to a binary file
-    std::ofstream compressed_file(compressed_file_name, std::ios:binary);
+    std::ofstream compressed_file(compressed_file_name, std::ios::binary);
     if (compressed_file) 
-        compressed_file.write(encoded_text_, size_file);
+        compressed_file.write(encoded_text_as_bytes, size_file);
     else 
         cout << "an error occured when trying to open the new file" << endl;
 
     // free the memory
-    delete[] encoded_text_;
+    delete[] encoded_text_as_bytes;
 }
 
-std::string HuffTree::retrieveEncodedText() {
-    
+std::string HuffmanCompress::decodeText(std::string compressed_file_name) {
+    std::ifstream compressed_file(compressed_file_name, std::ios::binary,
+                                  std::ios::ate);
+    if (compressed_file) {
+        // read the data into an array
+        size_t size_file = compressed_file.tellg();
+        unsigned char * encoded_text_as_bytes = new unsigned char [size_file];
+        compressed_file.seekg(0, std::ios::beg);
+        compressed_file.read(encoded_text_as_bytes, size_file);
+
+        // convert data to a string
+        std::string encoded_text = "";
+        for (size_t i = 0; i != size_file; i++)
+            encoded_text += 
+                HuffmanCompress::convertCharToBitstring(encoded_text_as_bytes[i]);
+
+        delete[] encoded_text_as_bytes;
+
+        // convert bitstring to text
+        text_ = "";
+        std::string word_or_sign;
+        for (size_t i = 0; i != encoded_text.size(); i++) {
+            if (decoding_map_.find(word_or_sign) == decoding_map_.end()) {
+                word_or_sign += encoded_text[i];
+            }
+            else {
+                text_ += decoding_map_[word_or_sign];
+                word_or_sign = "";
+            }
+        }
+        return text_;
+    }
+    else
+        return std::string("something went wrong");
 }
 
 
--- assignment_6/hufftree.h
+++ assignment_6/hufftree.h
@@ -22,31 +22,37 @@ public:
 	~PartHuffTree();
 
 	size_t getRootAmount() const;
-	static std::unique_ptr<PartHuffTree>& merge(PartHuffTree& first, PartHuffTree& second);
-	void erase();
+	friend std::unique_ptr<PartHuffTree>& merge(PartHuffTree& first, 
+												PartHuffTree& second);
+	void erase(HuffNode*& node);
 
 private:
-	HuffNode* root_;
+	HuffNode* root_ = nullptr;
 	size_t size_;
 	
-	friend class HuffTree;
+	friend class HuffmanCompress;
 };
 
 
 // Class that constructs a Huffman tree and encodes/decodes a file
-class HuffTree {
+class HuffmanCompress {
 public:
-	HuffTree();
-	~HuffTree();
+	HuffmanCompress();
+	~HuffmanCompress();
 
 	void countChars(std::string& file_name);
-	std::string convertCharToBitstring(unsigned char nr);
-	unsigned char convertBitstringToChar(std::string byte);
+	std::string convertCharToBitstring(unsigned char nr) const;
+	unsigned char convertBitstringToChar(std::string byte) const;
 	void makeEncodeTree();
-	void makeEncodeMap();
+	void makeEncodeDecodeMaps();
 	void encodeText();
-	void safeEncodedText() const;
-	std::string retrieveEncodedText();
+	void safeEncodedText(std::string compressed_file_name) const;
+
+	// retrieve the original text from the compressed file as a string.
+	// note: since this class goes out of scope when the program has finished,
+	// we need to save the encoding map somehow, otherwise we can't 
+	// decode the compressed file. 
+	std::string decodeText(std::string compressed_file_name);
 
 private:
 	PartHuffTree tree_;
@@ -54,6 +60,7 @@ private:
 	std::string encoded_text_ = "";
 	std::map<char, int> char_occurrences_;
 	std::map<char, std::string> encoding_map_;
+	std::map<std::string, char> decoding_map_;
 };
 
 }	// end namespace adt;
